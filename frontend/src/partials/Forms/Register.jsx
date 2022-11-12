4868090a9b317a47970ef98afeddfe820221f9cf
The <nguyenminhcuong2000@gmail.com>
2022-11-13 01:21:44 +0700

    #2022/11/13 cuong - User Authentication

--- frontend/src/partials/Forms/Register.jsx
+++ frontend/src/partials/Forms/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { TfiFacebook, TfiGoogle, TfiLinkedin } from "react-icons/tfi";
+import {useToast} from '@chakra-ui/react'
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -8,8 +9,21 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false)
+  const toast = useToast();
 
-  const postPic = (pics) => {};
+  const postPic = (pics) => {
+    setLoading(true)
+    if(pic === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom"
+      })
+    }
+  };
 
   const registerHandler = () => {};
   return (
