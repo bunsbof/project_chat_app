285a7620f3017a4b8593adf00f9749d94cb44a15
The <nguyenminhcuong2000@gmail.com>
2022-11-12 21:08:17 +0700

    #2022/11/12 cuong - Connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const chats = require("./data/data");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+const colors = require('colors')
 
 const app = express();
 dotenv.config()
+connectDB()
 
 app.get("/", (req, res) => {
   res.send("API is Running Successfully");
@@ -20,4 +23,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT =  process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server started at ${PORT}`));
+app.listen(PORT, console.log(`Server started at ${PORT}`.yellow.bold));
