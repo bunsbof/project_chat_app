4e8886a5a7061860ee9f3513004649cd2c62a115
The <nguyenminhcuong2000@gmail.com>
2022-11-14 03:58:01 +0700

    #2022/11/14 cuong - create ui and login, registration functionality

--- frontend/src/partials/Forms/Login.jsx
+++ frontend/src/partials/Forms/Login.jsx
@@ -1,16 +1,73 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { TfiFacebook, TfiGoogle, TfiLinkedin } from "react-icons/tfi";
+import { useToast } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const loginHandler = () => {};
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const loginHandler = async (e) => {
+    e.preventDefault()
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    // console.log(email, password);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      // console.log(JSON.stringify(data));
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={loginHandler}>
         <h1>Sign in</h1>
         <div className="social-container">
           <a className="social">
--- frontend/src/partials/Forms/Register.jsx
+++ frontend/src/partials/Forms/Register.jsx
@@ -1,31 +1,130 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { TfiFacebook, TfiGoogle, TfiLinkedin } from "react-icons/tfi";
-import {useToast} from '@chakra-ui/react'
+import { useToast } from "@chakra-ui/react";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
+  const [name, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [pic, setPic] = useState();
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const toast = useToast();
+  const navigate = useNavigate();
 
-  const postPic = (pics) => {
-    setLoading(true)
-    if(pic === undefined) {
+  const postDetails = (pics) => {
+    setLoading(true);
+    console.log(pics);
+    if (pics === undefined) {
       toast({
-        title: "Please Select an Image!",
+        title: "Please select an Image",
         status: "warning",
         duration: 5000,
         isClosable: true,
-        position: "bottom"
+        position: "top-left",
+      });
+      return;
+    }
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "do28v4lkm");
+      fetch("https://api.cloudinary.com/v1_1/do28v4lkm/image/upload", {
+        method: "post",
+        body: data,
       })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please select an Image",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      setLoading(false);
+      return;
     }
   };
 
-  const registerHandler = () => {};
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      setLoading(false);
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      toast({
+        title: "Registration Successfully",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <form onSubmit={registerHandler}>
@@ -42,17 +141,16 @@ const Register = () => {
           </a>
         </div>
         <input
-          value={pic}
           id="file"
           type="file"
           appcept="image/*"
-          onChange={(e) => setPassword(e.target.files[0])}
+          onChange={(e) => postDetails(e.target.files[0])}
         />
         <label htmlFor="file" className="forfile">
           Choose a Photo
         </label>
         <input
-          value={username}
+          value={name}
           type="text"
           placeholder="Name"
           onChange={(e) => setUsername(e.target.value)}
@@ -72,7 +170,7 @@ const Register = () => {
         <input
           value={confirmPassword}
           type="password"
-          placeholder="Password"
+          placeholder="Confirm Password"
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
         <button type="submit">Sign Up</button>
