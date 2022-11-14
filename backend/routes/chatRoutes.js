b7fa83428109d8ea7aaa7c68d2158d88fe75abb0
The <nguyenminhcuong2000@gmail.com>
2022-11-14 22:59:43 +0700

    #2022/11/14 cuong - Done Chat API for backend

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const { accessChat } = require("../controllers/chatControllers");
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeGroup } = require("../controllers/chatControllers");
 const { protect } = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
 router.route('/').post(protect, accessChat);
-// router.route('/').get(protect, fetchChats);
-// router.route('/group').post(protect, createGroupChat);
-// router.route('/rename').put(protect, renameGroupChat);
-// router.route('/removegroup').delete(protect, removeGroup);
-// router.route('/addgroup').put(protect, addToGroup);
+router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChat);
+router.route('/rename').put(protect, renameGroup);
+router.route('/addgroup').put(protect, addToGroup);
+router.route('/removegroup').put(protect, removeGroup);
 
 module.exports = router
