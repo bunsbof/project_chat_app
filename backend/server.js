f2091906483663cb2cf4c6979d2e432f23d6546d
The <nguyenminhcuong2000@gmail.com>
2022-11-14 19:53:38 +0700

    #2022/11/14 cuong - creating chats functionality backend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const chats = require("./data/data");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const colors = require('colors')
-const userRoutes = require('./routes/userRoutes')
-const {notFound, errorHandler} = require('./middleware/errorHandler')
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorHandler");
+const chatRoutes = require("./routes/chatRoutes");
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 const app = express();
 
-app.use(express.json()) // to accept JSON Data
+app.use(express.json()); // to accept JSON Data
 
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT =  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started at ${PORT}`.yellow.bold));
